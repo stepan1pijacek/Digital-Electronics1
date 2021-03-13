97660bdfddfaf5297c2344f5715a2d4469ac31af
Štěpán Pijáček <stepan.pijacek@gmail.com>
2021-03-13 10:58:08 +0100

    addingVivadoFiles

---
+++ LABS/05-counter16bit/counter/counter.srcs/sim_1/new/tb_clock_enable.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.03.2021 15:23:04
+// Design Name: 
+// Module Name: tb_clock_enable
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_clock_enable(
+
+    );
+endmodule
